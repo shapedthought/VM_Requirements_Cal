135ca6728f4578dfae36f2d16ccb19655ef481a9
Ed Howard <shapedthought@gmail.com>
2018-12-31 17:48:53 +0000

    Made a couple minor changes

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  outputItems = [];
+  pvmCPU: number;
+  pvmGHz: number;
+  pvmRam: number;
+
+  @Output() calSend = new EventEmitter<object>();
+
+  envReqForm = new FormGroup({
+    vmQty: new FormControl('', Validators.required),
+    vCPU: new FormControl('', Validators.required),
+    GHz: new FormControl('', Validators.required),
+    ram: new FormControl('', Validators.required)
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    const data = this.envReqForm.value;
+    this.pvmCPU = data.vCPU / data.vmQty;
+    this.pvmGHz = data.GHz / data.vmQty;
+    this.pvmRam = data.ram / data.vmQty;
+    this.outputItems.push(this.pvmCPU);
+    this.outputItems.push(this.pvmGHz);
+    this.outputItems.push(this.pvmRam);
+    this.calSend.emit(this.outputItems);
+    console.log(this.pvmCPU, this.pvmGHz, this.pvmRam);
+  }
+}
